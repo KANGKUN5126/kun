75aaba3a354a47c717793575f98ac4d4818541fa
KANGKUN5126 <chaos_5126@naver.com>
2024-05-29 16:40:32 +0900

    메인 페이지 DB 연동

--- kun/index.php
+++ kun/index.php
@@ -1,3 +1,30 @@
+<?
+include_once $_SERVER['DOCUMENT_ROOT'] . '/connect.php';
+$user_ip = $_SERVER['REMOTE_ADDR'];
+$query = "SELECT user_ip FROM user_binfo WHERE user_ip = :user_ip";
+$stmt = $connect->prepare($query);
+$stmt->bindParam(":user_ip", $user_ip);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($row) {
+    $user_ip = $row['user_ip'];
+} else {
+    echo '아이피가 존재하지 않습니다.';
+}
+
+$lunch_menu_arr = [];
+$lunch_menu_arr = [
+    '1' => '짜장면',
+    '2' => '냉면',
+    '3' => '김치찌개',
+    '4' => '돈가스',
+    '5' => '된장찌개',
+    '6' => '해장국',
+    '7' => '햄버거',
+    '8' => '초밥'
+];
+$lunch_menu = array_rand($lunch_menu_arr);
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,8 +34,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-test
-<?phpinfo()?>
+오늘의 점심메뉴 추천 :
+<?=$lunch_menu_arr[$lunch_menu]?>
 <body>
 </body>
 </html>
