8fa22fb584b36eded0e13a1795905a5fbb34cf4b
KANGKUN5126 <chaos_5126@naver.com>
2024-05-30 14:19:52 +0900

    세션 추가

--- kun/func/config.php
+++ kun/func/config.php
@@ -1,17 +1,26 @@
 <?
+session_set_cookie_params(86400); // 1일에 해당하는 초(60 * 60 * 24)
+session_start();
 # 점심메뉴
 $lunch_menu_arr = [];
 $lunch_menu_arr = [
-    '1' => '짜장면',
+    '1' => '중화요리',
     '2' => '냉면',
     '3' => '김치찌개',
     '4' => '돈가스',
     '5' => '된장찌개',
     '6' => '해장국',
     '7' => '햄버거',
-    '8' => '초밥'
+    '8' => '초밥',
+    '9' => '순대국',
+    '10' => '제육볶음',
+    '11' => '카레',
+    '12' => '분식',
 ];
 $lunch_menu = array_rand($lunch_menu_arr);
+if (!isset($_SESSION['lunch_menu'])) {
+    $_SESSION['lunch_menu'] = $lunch_menu_arr[$lunch_menu];
+}
 
 #유저 아이피
 $user_ip = $_SERVER['REMOTE_ADDR'];
--- kun/index.php
+++ kun/index.php
@@ -1,30 +1,35 @@
 <?
 include_once $_SERVER['DOCUMENT_ROOT'] . '/connect.php';
-$user_ip = $_SERVER['REMOTE_ADDR'];
-$query = "SELECT user_ip FROM user_binfo WHERE user_ip = :user_ip";
-$stmt = $connect->prepare($query);
-$stmt->bindParam(":user_ip", $user_ip);
-$stmt->execute();
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($row) {
-    $user_ip = $row['user_ip'];
-} else {
-    echo '아이피가 존재하지 않습니다.';
-}
-
-$lunch_menu_arr = [];
-$lunch_menu_arr = [
-    '1' => '짜장면',
-    '2' => '냉면',
-    '3' => '김치찌개',
-    '4' => '돈가스',
-    '5' => '된장찌개',
-    '6' => '해장국',
-    '7' => '햄버거',
-    '8' => '초밥'
-];
-$lunch_menu = array_rand($lunch_menu_arr);
+include_once $_SERVER['DOCUMENT_ROOT'] . '/func/config.php';
 ?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    function updateRemainingTime() {
+        var now = new Date();
+        var endOfWorkTime = new Date(now);
+        endOfWorkTime.setHours(18, 0, 0, 0); // 퇴근 시간을 설정합니다.
+
+        var remainingMilliseconds = endOfWorkTime - now;
+
+        if (remainingMilliseconds <= 0) {
+            $('.time').text('퇴근 시간이 지났습니다.');
+        } else {
+            var remainingHours = Math.floor(remainingMilliseconds / (1000 * 60 * 60));
+            remainingMilliseconds -= remainingHours * 1000 * 60 * 60;
+            var remainingMinutes = Math.floor(remainingMilliseconds / (1000 * 60));
+            remainingMilliseconds -= remainingMinutes * 1000 * 60;
+            var remainingSeconds = Math.floor(remainingMilliseconds / 1000);
+
+            var remainingTime = remainingHours + '시간 ' + remainingMinutes + '분 ' + remainingSeconds + '초';
+            $('.time').text('남은 퇴근 시간: ' + remainingTime);
+        }
+    }
+
+    // 매 초마다 퇴근 시간 업데이트
+    setInterval(updateRemainingTime, 1000);
+</script>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -34,8 +39,41 @@ $lunch_menu = array_rand($lunch_menu_arr);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-오늘의 점심메뉴 추천 :
-<?=$lunch_menu_arr[$lunch_menu]?>
+<?
+    $query = "SELECT user_ip FROM user_binfo WHERE user_ip = :user_ip";
+    $stmt = $connect->prepare($query);
+    $stmt->bindParam(":user_ip", $user_ip);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($row) {
+        $user_ip = $row['user_ip'];
+        echo "당신의 IP는 " . $user_ip . "입니다.";
+
+    } else {
+        $insert_query = "insert into user_binfo set user_ip = :user_ip";
+        $stmt = $connect->prepare($insert_query);
+        $stmt->bindParam(":user_ip", $user_ip);
+        $stmt->execute();
+    }
+?>
+<br>
+
 <body>
+<?
+    $visit_query = "select count(distinct user_ip) as ip_count from user_binfo ";
+    $stmt = $connect->prepare($visit_query);
+    $stmt->execute();
+    $visit_count = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<p>
+    총 방문자 수 : <?= $visit_count['ip_count'] ?>
+</p>
+
+오늘의 점심메뉴 추천 :
+<?= $_SESSION['lunch_menu'] ?>
+    <div class="time">
+        남은 퇴근 시간 :
+    </div>
 </body>
 </html>
